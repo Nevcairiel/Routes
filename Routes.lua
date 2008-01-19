a6c9e8d7cab52cdccb3aad3c9853a90757902a0b
Xinhuan <xinhuan@pacific.net.sg>
2008-01-19 23:22:04 +0000

    Copy Graph functions over

--- Routes.lua
+++ Routes.lua
@@ -3,11 +3,94 @@
 Routes = LibStub("AceAddon-3.0"):NewAddon("Routes", "AceConsole-3.0", "AceEvent-3.0")
 local Routes = Routes
 local L = LibStub("AceLocale-3.0"):GetLocale("Routes", false)
+local G = {} -- was Graph-1.0, but we removed the dependency
 
 function Routes:OnEnable()
 end
 
 function Routes:OnDisable()
+end
+
+
+
+
+
+------------------------------------------------------------------------------------------------------
+-- The following function is used with permission from Daniel Stephens <iriel@vigilance-committee.org>
+-- with reference to TaxiFrame.lua in Blizzard's UI and Graph-1.0 Ace2 library (by Cryect) which I now
+-- maintain after porting it to LibGraph-2.0 LibStub library -- Xinhuan
+local TAXIROUTE_LINEFACTOR = 128/126; -- Multiplying factor for texture coordinates
+local TAXIROUTE_LINEFACTOR_2 = TAXIROUTE_LINEFACTOR / 2; -- Half of that
+
+-- T        - Texture
+-- C        - Canvas Frame (for anchoring)
+-- sx,sy    - Coordinate of start of line
+-- ex,ey    - Coordinate of end of line
+-- w        - Width of line
+-- relPoint - Relative point on canvas to interpret coords (Default BOTTOMLEFT)
+function G:DrawLine(C, sx, sy, ex, ey, w, color, layer)
+	local relPoint = "BOTTOMLEFT"
+	
+	if not C.Routes_Lines then
+		C.Routes_Lines={}
+		C.Routes_Lines_Used={}
+	end
+
+	local T = tremove(C.Routes_Lines) or C:CreateTexture(nil, "ARTWORK")
+	T:SetTexture("Interface\\AddOns\\Routes\\line")
+	tinsert(C.Routes_Lines_Used,T)
+
+	T:SetDrawLayer(layer or "ARTWORK")
+
+	T:SetVertexColor(color[1],color[2],color[3],color[4]);
+	-- Determine dimensions and center point of line
+	local dx,dy = ex - sx, ey - sy;
+	local cx,cy = (sx + ex) / 2, (sy + ey) / 2;
+
+	-- Normalize direction if necessary
+	if (dx < 0) then
+		dx,dy = -dx,-dy;
+	end
+
+	-- Calculate actual length of line
+	local l = sqrt((dx * dx) + (dy * dy));
+
+	-- Sin and Cosine of rotation, and combination (for later)
+	local s,c = -dy / l, dx / l;
+	local sc = s * c;
+
+	-- Calculate bounding box size and texture coordinates
+	local Bwid, Bhgt, BLx, BLy, TLx, TLy, TRx, TRy, BRx, BRy;
+	if (dy >= 0) then
+		Bwid = ((l * c) - (w * s)) * TAXIROUTE_LINEFACTOR_2;
+		Bhgt = ((w * c) - (l * s)) * TAXIROUTE_LINEFACTOR_2;
+		BLx, BLy, BRy = (w / l) * sc, s * s, (l / w) * sc;
+		BRx, TLx, TLy, TRx = 1 - BLy, BLy, 1 - BRy, 1 - BLx; 
+		TRy = BRx;
+	else
+		Bwid = ((l * c) + (w * s)) * TAXIROUTE_LINEFACTOR_2;
+		Bhgt = ((w * c) + (l * s)) * TAXIROUTE_LINEFACTOR_2;
+		BLx, BLy, BRx = s * s, -(l / w) * sc, 1 + (w / l) * sc;
+		BRy, TLx, TLy, TRy = BLx, 1 - BRx, 1 - BLx, 1 - BLy;
+		TRx = TLy;
+	end
+
+	-- Set texture coordinates and anchors
+	T:ClearAllPoints();
+	T:SetTexCoord(TLx, TLy, BLx, BLy, TRx, TRy, BRx, BRy);
+	T:SetPoint("BOTTOMLEFT", C, relPoint, cx - Bwid, cy - Bhgt);
+	T:SetPoint("TOPRIGHT",   C, relPoint, cx + Bwid, cy + Bhgt);
+	T:Show()
+	return T
+end
+
+function G:HideLines(C)
+	if C.Routes_Lines then
+		for i = #C.Routes_Lines_Used, 1, -1 do
+			C.Routes_Lines_Used[i]:Hide()
+			tinsert(C.Routes_Lines,tremove(C.Routes_Lines_Used))
+		end
+	end
 end
 
 -- vim: ts=4 noexpandtab

6999c1b0582fe4433d663762022836a54f41d5c3
Xinhuan <xinhuan@pacific.net.sg>
2008-01-22 21:32:43 +0000

    Move Auto Show/Hide into a submodule

--- Routes.lua
+++ Routes.lua
@@ -113,24 +113,6 @@ local defaults = {
 	}
 }
 
-local prof_options = {
-	["Always"] = L["Always show"],
-	["With Profession"] = L["Only with profession"],
-	["When active"] = L["Only while tracking"],
-	["Never"] = L["Never show"],
-}
-local prof_options2 = { -- For Treasure, which isn't a profession
-	["Always"] = L["Always show"],
-	["When active"] = L["Only while tracking"],
-	["Never"] = L["Never show"],
-}
-local prof_options3 = { -- For Gas, which doesn't have tracking as a skill
-	["Always"] = L["Always show"],
-	["With Profession"] = L["Only with profession"],
-	["Never"] = L["Never show"],
-}
-
-
 -- Ace Options Table for our addon
 local options
 -- Plugins table
@@ -754,99 +736,12 @@ function Routes:OnEnable()
 		minimap_rotate = GetCVar("rotateMinimap") == "1"
 		self:MINIMAP_UPDATE_ZOOM()  -- This has a DrawMinimapLines(true) call in it, and sets an "indoors" variable
 	end
-	if db.defaults.use_auto_showhide then
-		Routes:RegisterEvent("SKILL_LINES_CHANGED")
-		Routes:RegisterEvent("MINIMAP_UPDATE_TRACKING")
-		Routes:MINIMAP_UPDATE_TRACKING()
-		Routes:SKILL_LINES_CHANGED()
-	else
-		local t = options.args.options_group.args.auto_group.args.auto_group.args
-		t.fishing.disabled = true
-		t.herbalism.disabled = true
-		t.mining.disabled = true
-		t.treasure.disabled = true
-		t.gas.disabled = true
-	end
 	self:SetupSourcesOptTables()
 end
 
 function Routes:OnDisable()
 	-- Ace3 unregisters all events and hooks for us on disable
 	timerFrame:Hide()
-end
-
-do
-	local have_prof = {
-		Herbalism = false,
-		Mining = false,
-		Fishing = false,
-		ExtractGas = false, -- Engineering
-	}
-	local texture_to_profession = {
-		["Interface\\Icons\\Spell_Nature_Earthquake"] = "Mining",
-		["Interface\\Icons\\INV_Misc_Flower_02"] = "Herbalism",
-		["Interface\\Icons\\INV_Misc_Fish_02"] = "Fishing",
-		["Interface\\Icons\\Racial_Dwarf_FindTreasure"] = "Treasure",
-	}
-	local active_tracking
-
-	function Routes:SKILL_LINES_CHANGED()
-		local skillname, isHeader
-		for i = 1, GetNumSkillLines() do
-			skillname, isHeader = GetSkillLineInfo(i)
-			if not isHeader and skillname then
-				if strfind(skillname, L["Skill-Fishing"]) then
-					have_prof.Fishing = true
-				elseif strfind(skillname, L["Skill-Herbalism"]) then
-					have_prof.Herbalism = true
-				elseif strfind(skillname, L["Skill-Mining"]) then
-					have_prof.Mining = true
-				elseif strfind(skillname, L["Skill-Engineering"]) then
-					have_prof.ExtractGas = true
-				end
-			end
-		end
-		self:ApplyVisibility()
-	end
-
-	function Routes:MINIMAP_UPDATE_TRACKING()
-		active_tracking = texture_to_profession[GetTrackingTexture()]
-		self:ApplyVisibility()
-	end
-
-	function Routes:ApplyVisibility()
-		local modified = false
-		for zone, zone_table in pairs(db.routes) do -- for each zone
-			if next(zone_table) ~= nil then
-				for route_name, route_data in pairs(zone_table) do -- for each route
-					if route_data.db_type then
-						local visible = false
-						for db_type in pairs(route_data.db_type) do -- for each db type used
-							local status = db.defaults.prof_options[db_type]
-							if status == "Always" then
-								visible = true
-							elseif status == "With Profession" and have_prof[db_type] then
-								visible = true
-							elseif status == "When active" and active_tracking == db_type then
-								visible = true
-							--elseif status == "Never" then
-							--	visible = false
-							end
-							if not visible == not route_data.visible then
-								modified = true
-							end
-							route_data.visible = visible
-						end
-					end
-				end
-			end
-		end
-		if modified then
-			-- redraw worldmap + minimap
-			self:DrawWorldmapLines()
-			self:DrawMinimapLines(true)
-		end
-	end
 end
 
 
@@ -1027,95 +922,6 @@ options.args.options_group.args = {
 			},
 		},
 	},
-	auto_group = {
-		name = L["Auto show/hide"], type = "group",
-		desc = L["Auto show and hide routes based on your professions"],
-		--groupType = "inline",
-		order = 200,
-		args = {
-			use_auto_showhide = {
-				name = L["Use Auto Show/Hide"],
-				desc = L["Use Auto Show/Hide"],
-				type = "toggle",
-				arg = "use_auto_showhide",
-				order = 210,
-				set = function(info, v)
-					db.defaults.use_auto_showhide = v
-					local t = options.args.options_group.args.auto_group.args.auto_group.args
-					if v then
-						Routes:RegisterEvent("SKILL_LINES_CHANGED")
-						Routes:RegisterEvent("MINIMAP_UPDATE_TRACKING")
-						Routes:MINIMAP_UPDATE_TRACKING()
-						Routes:SKILL_LINES_CHANGED()
-						t.fishing.disabled = nil
-						t.herbalism.disabled = nil
-						t.mining.disabled = nil
-						t.treasure.disabled = nil
-						t.gas.disabled = nil
-					else
-						Routes:UnregisterEvent("SKILL_LINES_CHANGED")
-						Routes:UnregisterEvent("MINIMAP_UPDATE_TRACKING")
-						t.fishing.disabled = true
-						t.herbalism.disabled = true
-						t.mining.disabled = true
-						t.treasure.disabled = true
-						t.gas.disabled = true
-					end
-					Routes:DrawWorldmapLines()
-					Routes:DrawMinimapLines(true)
-				end,
-			},
-			auto_group = {
-				name = L["Auto Show/Hide per route type"], type = "group",
-				desc = L["Auto Show/Hide settings"],
-				inline = true,
-				order = 300,
-				disabled = function(info) return not db.defaults.use_auto_showhide end,
-				set = function(info, v)
-					db.defaults.prof_options[info.arg] = v
-					Routes:ApplyVisibility()
-				end,
-				get = function(info) return db.defaults.prof_options[info.arg] end,
-				args = {
-					fishing = {
-						name = L["Fishing"], type = "select",
-						desc = L["Routes with Fish"],
-						order = 100,
-						values = prof_options,
-						arg = "Fishing",
-					},
-					gas = {
-						name = L["ExtractGas"], type = "select",
-						desc = L["Routes with Gas"],
-						order = 200,
-						values = prof_options3,
-						arg = "ExtractGas",
-					},
-					herbalism = {
-						name = L["Herbalism"], type = "select",
-						desc = L["Routes with Herbs"],
-						order = 300,
-						values = prof_options,
-						arg = "Herbalism",
-					},
-					mining = {
-						name = L["Mining"], type = "select",
-						desc = L["Routes with Ore"],
-						order = 400,
-						values = prof_options,
-						arg = "Mining",
-					},
-					treasure = {
-						name = L["Treasure"], type = "select",
-						desc = L["Routes with Treasure"],
-						order = 500,
-						values = prof_options2,
-						arg = "Treasure",
-					},
-				},
-			},
-		},
-	},
 }
 
 
@@ -1236,7 +1042,10 @@ function ConfigHandler:DoForeground(info)
 	Routes:Print(L["Path with %d nodes found with length %.2f yards after %d iterations in %.2f seconds."]:format(#output, length, iter, timetaken))
 
 	-- redraw lines
-	Routes:ApplyVisibility()
+	local AutoShow = Routes:GetModule("AutoShow", true)
+	if AutoShow and db.defaults.use_auto_showhide then
+		AutoShow:ApplyVisibility()
+	end
 	Routes:DrawWorldmapLines()
 	Routes:DrawMinimapLines(true)
 end
@@ -1251,7 +1060,10 @@ function ConfigHandler:DoBackground(info)
 			t.length = length
 			Routes:Print(L["Path with %d nodes found with length %.2f yards after %d iterations in %.2f seconds."]:format(#output, length, iter, timetaken))
 			-- redraw lines
-			Routes:ApplyVisibility()
+			local AutoShow = Routes:GetModule("AutoShow", true)
+			if AutoShow and db.defaults.use_auto_showhide then
+				AutoShow:ApplyVisibility()
+			end
 			Routes:DrawWorldmapLines()
 			Routes:DrawMinimapLines(true)
 		end)

47fc84b5d63415ce1772023c9be618ab8743bea1
Xinhuan <xinhuan@pacific.net.sg>
2008-06-03 05:57:31 +0000

    Enable TomTom waypoints and crazyarrow support. Requires TomTom r145 or newer.

--- Modules/TomTom.lua
+++ Modules/TomTom.lua
@@ -1,8 +1,4 @@
-﻿do
-	return
-end
-
-local Routes = LibStub("AceAddon-3.0"):GetAddon("Routes")
+﻿local Routes = LibStub("AceAddon-3.0"):GetAddon("Routes")
 local TT = Routes:NewModule("TomTom", "AceEvent-3.0")
 local L = LibStub("AceLocale-3.0"):GetLocale("Routes")
 
